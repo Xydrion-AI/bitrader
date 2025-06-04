fe65768bb58f1b965300448812bcbe5a49af682a
Opallym <gondremaelle@gmail.com>
2025-06-05 00:33:21 +0200

    team home et details

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TeamsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('pages/home/home.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+public function index(TeamsRepository $repo): Response
+{
+    $teamMembers = $repo->findAll();
+
+    return $this->render('pages/home/home.html.twig', [
+        'controller_name' => 'HomeController',
+        'teamMembers' => $teamMembers,
+    ]);
+}
+
 }
